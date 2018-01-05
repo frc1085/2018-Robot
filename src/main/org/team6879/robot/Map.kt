4ba6b51559521304d42f0b64192c1a1c0f9c8d75
Shmish <shmish90@gmail.com>
2018-01-05 14:43:52 -0500

    added dashboard

--- src/main/org/team6879/robot/Map.kt
+++ src/main/org/team6879/robot/Map.kt
@@ -1,5 +1,10 @@
 object Map {
+  
   // PWM Channels
   public leftTalonChannel = 0
   public rightTalonChannel = 1
+
+  // HID Ports
+  public leftStickPort = 0
+  public rightStickPort = 1
 }
---
+++ src/main/org/team6879/robot/subsystems/Dashboard.kt
@@ -0,0 +1,18 @@
+package org.team6879.robot.subsystems
+
+import edu.wpi.first.wpilibj.command.Subsystem
+import edu.wpi.first.wpilibj.Joystick
+import edu.wpi.first.wpilibj.JoystickButton
+import edu.wpi.first.wpilibj.networktables.NetworkTable
+import org.team6879.robot.Map
+
+class Dashboard : Subsystem {
+
+  private val visionTable = NetworkTable.getTable("vision")
+  private val sensorTable = NetworkTable.getTable("sensor")
+  private val errorsTable = NetworkTable.getTable("errors")
+
+  private val leftStick = Joystick(Map.leftStickPort)
+  private val rightStick = Joystick(Map.rightStickPort)
+
+}
