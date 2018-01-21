ae99b417dbdfb77ee111b16ad2dbe0566ed429e2
Shmish <shmish90@gmail.com>
2018-01-21 10:37:32 -0500

    added auto selector

--- src/main/kotlin/org/team6879/robot/subsystems/Dashboard.kt
+++ src/main/kotlin/org/team6879/robot/subsystems/Dashboard.kt
@@ -4,10 +4,13 @@ import edu.wpi.first.wpilibj.command.Subsystem
 import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.buttons.JoystickButton
 import edu.wpi.first.networktables.NetworkTableInstance
+import edu.wpi.first.wpilibj.DriverStation
 import org.team6879.robot.Map
 import org.team6879.robot.commands.Logging
 
 class Dashboard : Subsystem () {
+
+  private val station = DriverStation.getInstance()
 
   private val globalTable = NetworkTableInstance.create()
   private val visionTable = globalTable.getTable("vision")
@@ -19,59 +22,10 @@ class Dashboard : Subsystem () {
 
   override fun initDefaultCommand () = setDefaultCommand(Logging())
 
-  // public fun getAuto (x: String): String = when (x) {
-  //   "LLL" -> "LLL",
-  //   else -> "LLL"
-  // }
+  public fun selectAuto (): String = when (station.getGameSpecificMessage()) {
+    "LLL" -> "lll"
+    "LLR" -> "llr"
+    else -> "other"
+  }
 
 }
-
-// Don't touch anything below this line, 
-// it's critical to robot operation
-// (i see you Walnut) -Shmish
-
-/*
-
-
-
-                                          `-.`'.-'
-                                       `-.        .-'.
-                                    `-.    -./\.-    .-'
-                                        -.  /_|\  .-
-                                    `-.   `/____\'   .-'.
-                                 `-.    -./.-""-.\.-      '
-                                    `-.  /< (()) >\  .-'
-                                  -   .`/__`-..-'__\'   .-
-                                ,...`-./___|____|___\.-'.,.
-                                   ,-'   ,` . . ',   `-,
-                                ,-'   ________________  `-,
-                                   ,'/____|_____|_____\
-                                  / /__|_____|_____|___\
-                                 / /|_____|_____|_____|_\
-                                ' /____|_____|_____|_____\
-                              .' /__|_____|_____|_____|___\
-                             ,' /|_____|_____|_____|_____|_\
-,,---''--...___...--'''--.. /../____|_____|_____|_____|_____\ ..--```--...___...--``---,,
-                           '../__|_____|_____|_____|_____|___\
-      \    )              '.:/|_____|_____|_____|_____|_____|_\               (    /
-      )\  / )           ,':./____|_____|_____|_____|_____|_____\             ( \  /(
-     / / ( (           /:../__|_____|_____|_____|_____|_____|___\             ) ) \ \
-    | |   \ \         /.../|_____|_____|_____|_____|_____|_____|_\           / /   | |
- .-.\ \    \ \       '..:/____|_____|_____|_____|_____|_____|_____\         / /    / /.-.
-(=  )\ `._.' |       \:./ _  _ ___  ____ ____ _    _ _ _ _ _  _ ___\        | `._.' /(  =)
- \ (_)       )       \./  |\/| |__) |___ |___ |___ _X_ _X_  \/  _|_ \       (       (_) /
-  \    `----'         """"""""""""""""""""""""""""""""""""""""""""""""       `----'    /
-   \   ____\__          __ __    _  __ _     _  __ ________    _____        __/____   /
-    \ (=\     \        (_ |_ |V||_)|_ |_)   |_|(_ /   |  |    |_  |        /     /-) /
-     \_)_\     \       __)|__| ||  |__| \   | |__)\___|__|_   |  _|_      /     /_(_/
-          \     \                                                        /     /
-           )     )  _                                                _  (     (
-          (     (,-' `-..__                                    __..-' `-,)     )
-           \_.-''          ``-..____                  ____..-''          ``-._/
-            `-._                    ``--...____...--''                    _.-'
-                `-.._                                                _..-'
-                     `-..__   I Wonder Who Put This Here       __..-'
-                           ``-..____                  ____..-''
-                                    ``--...____...--''
-
-*/
