52764eca3701673ad035192af050bf693667407b
Walden Yan <walnutwaldo20@gmail.com>
2018-02-03 14:07:18 -0500

    Create OI.java

---
+++ src/main/Java/org/team1085/robot/OI.java
@@ -0,0 +1,14 @@
+package org.team1085.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class OI {
+	
+	public static Joystick controller1 = new Joystick(Map.CONTROLLER_1_PORT);
+	public static Joystick controller2 = new Joystick(Map.CONTROLLER_2_PORT);
+
+	
+	public OI() {
+	}
+	
+}
