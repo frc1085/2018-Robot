b123ff73c27ac58f7af6e526452a50a55c00ee50
Walden Yan <walnutwaldo20@gmail.com>
2018-02-03 14:07:05 -0500

    Update Map.java

--- src/main/Java/org/team1085/robot/Map.java
+++ src/main/Java/org/team1085/robot/Map.java
@@ -2,7 +2,7 @@ package org.team1085.robot;
 
 public class Map {
 
-	// PWM Channels
+	// PWM
 	public static final int RIGHT_CIM_FRONT = 0;
 	public static final int RIGHT_CIM_BACK = 1;
 	public static final int RIGHT_CIM_MINI = 2;
@@ -11,15 +11,23 @@ public class Map {
 	public static final int LEFT_CIM_BACK = 4;
 	public static final int LEFT_CIM_MINI = 5;
 
-	// DIO Channels
+	public static final boolean LEFT_INVERTED = true;
+	public static final boolean RIGHT_INVERTED = false;
+
+	// DIO
 	public static final int RIGHT_ENCODER_A = 0;
 	public static final int RIGHT_ENCODER_B = 1;
 
 	public static final int LEFT_ENCODER_A = 2;
 	public static final int LEFT_ENCODER_B = 3;
 
-	// IO Channels
-	public static final int LEFT_STICK_PORT = 0;
-	public static final int RIGHT_STICK_PORT = 1;
+	// OI
+	public static final int CONTROLLER_1_PORT = 0; // Controls chassis and intake
+	public static final int CONTROLLER_2_PORT = 1; // Controls arm
+
+	public static final int LEFT_X_AXIS = 0;
+	public static final int LEFT_Y_AXIS = 1;
+	public static final int RIGHT_X_AXIS = 4;
+	public static final int RIGHT_Y_AXIS = 5;
 
 }
