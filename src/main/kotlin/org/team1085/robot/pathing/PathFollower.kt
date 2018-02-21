5ba5a625246631db87652f472506efdb47df36e2
Shmish <shmish90@gmail.com>
2018-02-20 21:41:35 -0500

    Added left and right paths

--- src/main/kotlin/org/team1085/robot/pathing/PathFollower.kt
+++ src/main/kotlin/org/team1085/robot/pathing/PathFollower.kt
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.command.Command
 import jaci.pathfinder.Pathfinder
 import jaci.pathfinder.Trajectory
 import jaci.pathfinder.Waypoint
+import jaci.pathfinder.modifiers.TankModifier;
 import jaci.pathfinder.followers.EncoderFollower
 
 class PathFollower (vararg coords: Triple<Double, Double, Double>) : Command () {
@@ -11,6 +12,10 @@ class PathFollower (vararg coords: Triple<Double, Double, Double>) : Command () 
   private val config = Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0)
   private val points = coords.map { coord -> Waypoint(coord.first, coord.second, coord.third) }.toSet().toTypedArray()
   private val center = Pathfinder.generate(points, config)
+  private val modify = TankModifier(center).modify(0.635)
+
+  private val left = modify.getLeftTrajectory()
+  private val right = modify.getRightTrajectory()
 
   override fun isFinished () = false
 }
