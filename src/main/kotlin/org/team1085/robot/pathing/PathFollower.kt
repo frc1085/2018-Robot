93775398368362bb128817a25d397375d7fc9538
Shmish <shmish90@gmail.com>
2018-02-20 22:04:37 -0500

    Removed redundant .set()

--- src/main/kotlin/org/team1085/robot/pathing/PathFollower.kt
+++ src/main/kotlin/org/team1085/robot/pathing/PathFollower.kt
@@ -11,7 +11,7 @@ import org.team1085.robot.Robot
 class PathFollower (vararg coords: Triple<Double, Double, Double>) : Command () {
 
   private val config = Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0)
-  private val points = coords.map { coord -> Waypoint(coord.first, coord.second, coord.third) }.toSet().toTypedArray()
+  private val points = coords.map { coord -> Waypoint(coord.first, coord.second, coord.third) }.toTypedArray()
   private val center = Pathfinder.generate(points, config)
   private val modify = TankModifier(center).modify(0.635)
 
